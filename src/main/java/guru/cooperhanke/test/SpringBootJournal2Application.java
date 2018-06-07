1aa0f81b75307378c20a2dbd5cd9fb12b1eabcf7
Cooper Hanke <cooper.hanke@gmail.com>
2018-06-07 15:21:59 -0500

    init commit before posting to public

---
+++ src/main/java/guru/cooperhanke/test/SpringBootJournal2Application.java
@@ -0,0 +1,12 @@
+package guru.cooperhanke.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJournal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJournal2Application.class, args);
+	}
+}
